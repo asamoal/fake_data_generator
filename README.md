bec016cfe8604b1c92b1a351a37a3ddd9fefe984
Leslie @Data Mavens Ltd <2324551+asamoal@users.noreply.github.com>
2023-12-15 01:32:21 +0100

    Add timing metrics and enhance README
    This commit makes several enhancements to the fake data generator script:
    
    - Added timing metrics to the script that measure the time taken to generate each individual file and the total time for generating all requested files. The timing metrics provide useful information for performance monitoring and optimization.
    
    - Enhanced the argument parsing to display help message when no arguments or incorrect arguments are provided.
    
    - Updated the manifest and checksum file generation to only add the file name, not the full directory path.
    
    - Updated the README file to reflect these updates and improve overall instructions for usage.
    
    These updates help provide better usability, performance monitoring, and documentation for the script.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ You can run the script from the command line, specifying the number of files to 
 
 Example:
 
-```bash python3 generate_files.py --num_files 10 --size 10MB --pattern dataset```
+```bash python3 data_generator.py --num_files 10 --size 10MB --pattern dataset```
 
 This command will generate a directory named 'dataset' containing 10 files of 1MB each, named `dataset_001.txt`, `dataset_002.txt`, etc. The directory will also contain a `dataset_manifest.txt` file listing the details of all generated files and a `dataset_checksums.txt` file providing the MD5 and SHA256 checksums of each file.
 
@@ -17,11 +17,21 @@ Arguments:
 - `--size` - The size of each file, which could be specified in bytes, or with units KB, MB, GB (e.g. 1KB, 1MB, 1GB)
 - `--pattern` - The naming pattern for the files
 
+Arguments are required. If the script is run without any arguments or with incorrect arguments, it will display an error message along with the help message.
+
 ## Files generated
-1. Dataset files: Contain randomly generated fake text data.
-2. Manifest file: A TXT file listing all the files in the dataset, their sizes, and creation dates. Named after the pattern provided.
-3. Checksum file: A TXT file containing the MD5 and SHA256 checksums (in Hex and Base64) of each file in the dataset.
+1. Dataset files: Contain randomly generated fake text data. Sizes are specific to what user has passed in the `--size` argument.
+2. Manifest file: A TXT file listing all the files in the dataset, their sizes in human readable format (bytes, KB, MB, GB etc.), and creation dates. Named after the pattern provided. Only contains the file name, not the full path.
+3. Checksum file: A TXT file containing the MD5 and SHA256 checksums (in Hex and Base64) of each file in the dataset. Only contains the file name, not the full path.
+
+## Timing the File Creation
+
+As of recent updates, the script now includes timers that measure the time taken to generate each individual file and the total duration for the script to generate all requested files. This can help with performance monitoring and optimization. 
+
+The timing for the creation of each file is displayed as the file is created. Upon completion of all files, the script will display the total size of the generated data and the total time taken. 
+
+Times are displayed in a human readable format, as milliseconds, seconds, minutes, hours, or days, depending on the magnitude of the duration. 
 
 ## Limitations
 
-This script does not perform error checking, and assumes that the user has appropriate permissions to write all these files to the current directory. Additionally, it uses very simple methods of generating dummy file contents and computing checksums.
+This script assumes that the user has appropriate permissions to write all these files to the current directory.
--- data_generator.py
+++ data_generator.py
@@ -3,17 +3,28 @@ import argparse
 import os
 import shutil
 import base64
+import sys
+import time
 import datetime
 from faker import Faker
 
+
+class CustomArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+
 def parse_size(size_str):
     # default to bytes if no unit is provided
-    units = {'KB': 1024, 'MB': 1024*1024, 'GB': 1024*1024*1024}
+    units = {'KB': 1024, 'MB': 1024 * 1024, 'GB': 1024 * 1024 * 1024}
     size_str = size_str.upper()
     if size_str[-2:] in units:
         return int(size_str[:-2]) * units[size_str[-2:]]
     else:  # Default is bytes
         return int(size_str)
+
 
 def compute_checksum(file_name, algorithm):
     with open(file_name, 'rb') as f:
@@ -26,16 +37,40 @@ def compute_checksum(file_name, algorithm):
     return checksum.hexdigest(), base64.b64encode(checksum.digest()).decode()
 
 
+def human_time_span(time_span):
+    parts = datetime.timedelta(milliseconds=time_span)
+    if parts < datetime.timedelta(seconds=1):
+        return str(parts.total_seconds() * 1000) + " milliseconds"
+    elif parts < datetime.timedelta(minutes=5):
+        return str(parts.total_seconds()) + " seconds"
+    elif parts < datetime.timedelta(hours=1):
+        return str(parts.total_seconds() // 60) + " minutes"
+    elif parts < datetime.timedelta(days=1):
+        return str(parts.total_seconds() // 3600) + " hours"
+    else:
+        return str(parts.total_seconds() // 86400) + " days"
+
+
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    step_unit = 1000.0  # 1024 bad the size
+
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < step_unit:
+            return "%3.1f %s" % (num, x)
+        num /= step_unit
+
+
 def generate_files(num_files, file_size, name_pattern):
     fake = Faker()
-
 
     manifest_file = ""
     checksum_file = ""
     manifest = []
     checksums = {}
 
-    # Check if directory exists
     if os.path.isdir(name_pattern):
         # Remove all files in directory
         for filename in os.listdir(name_pattern):
@@ -47,21 +82,27 @@ def generate_files(num_files, file_size, name_pattern):
                     shutil.rmtree(file_path)
             except Exception as e:
                 print(f'Failed to delete {file_path}. Reason: {e}')
+
     else:
-        # If directory does not exist, create it
         os.makedirs(name_pattern, exist_ok=True)
 
+    total_start_time = time.time() * 1000
+    total_size = 0
+
     for i in range(1, num_files + 1):
+        single_start_time = time.time() * 1000
+
         file_name = os.path.join(name_pattern, f"{name_pattern}-{i:03}.txt")
         manifest_file = os.path.join(name_pattern, f"{name_pattern}_manifest.txt")
         checksum_file = os.path.join(name_pattern, f"{name_pattern}_checksums.txt")
 
-        # Generate fake data
         data = fake.text(file_size)[:file_size]
 
         with open(file_name, 'w') as f:
             f.write(data)
             manifest.append(file_name)
+
+        total_size += os.path.getsize(file_name)
 
         with open(file_name, 'rb'):
             checksums[file_name] = {
@@ -69,31 +110,41 @@ def generate_files(num_files, file_size, name_pattern):
                 'sha256': compute_checksum(file_name, 'sha256')
             }
 
+        single_end_time = time.time() * 1000
+        print(f"Time taken to create file-{i}: {human_time_span(single_end_time - single_start_time)}")
+
     with open(manifest_file, 'w') as f:
-        f.write('FileName,FileSize,CreationDate\n')
         for file_name in manifest:
-            size = os.path.getsize(file_name)
+            size = convert_bytes(os.path.getsize(file_name))  # use convert_bytes here
             creation_date = datetime.datetime.utcfromtimestamp(os.path.getctime(file_name)).isoformat() + 'Z'
-            f.write(f'{file_name},{size},{creation_date}\n')
+            f.write(f'{os.path.basename(file_name)},{size},{creation_date}\n')
 
     with open(checksum_file, 'w') as f:
-        f.write('FileName,MD5,MD5_Base64,SHA256,SHA256_Base64\n')
         for filename, checksum_values in checksums.items():
             f.write(
-                f'{filename},{checksum_values["md5"][0]},{checksum_values["md5"][1]},{checksum_values["sha256"][0]},{checksum_values["sha256"][1]}\n')
+                f'{os.path.basename(filename)},{checksum_values["md5"][0]},{checksum_values["md5"][1]},'
+                f'{checksum_values["sha256"][0]},{checksum_values["sha256"][1]}\n')
 
-    print(f"Generated {num_files} files of {file_size} bytes each, "
+    total_end_time = time.time() * 1000
+    print(f"Generated {num_files} files of {convert_bytes(file_size)} each, "  # use convert_bytes here
           f"with names following the pattern {name_pattern}.")
+    print(f"Total size: {convert_bytes(total_size)}")
+    print(f"Total time taken: {human_time_span(total_end_time - total_start_time)}")
 
 
 # Set up argument parser
-parser = argparse.ArgumentParser()
+parser = CustomArgumentParser()
 parser.add_argument("-n", "--num_files", type=int, help="number of files to generate")
 parser.add_argument("-s", "--size", type=str, help="size of each file in KB, MB or GB")
 parser.add_argument("-p", "--pattern", type=str, help="name pattern for the files")
 
 # Parse arguments
 args = parser.parse_args()
+
+# it also makes sense to check if the expected arguments are actually provided
+expected_args = [args.num_files, args.size, args.pattern]
+if not all(expected_args):
+    parser.error("Unexpected set of arguments, please provide --num_files, --size, and --pattern.")
 
 # Call function
 size_in_bytes = parse_size(args.size)
